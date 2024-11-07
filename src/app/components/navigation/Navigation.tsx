9bfb3ebc4e47ffd1c91d1c42901216d3e3d812ab
Alexandra Pierson <alexandrajpierson@gmail.com>
2024-11-06 21:33:03 -0600

    setup content grid and navigation

---
+++ src/app/components/navigation/Navigation.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './navigation.module.css'
+
+const Navigation = () => {
+  return (
+    <nav className='full-width'>
+      <ul className={styles.navigation}>
+        <li>About</li>
+        <li>Services</li>
+        <li>Cherished Moments</li>
+        <li>Porfolio</li>
+        <li>Contact</li>
+        <li>Book Now</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,47 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css'
+import Navigation from './components/navigation/Navigation'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className='content-grid'>
+      <Navigation />
+      <main>
+        <img
+          src='/images/bridal-flowers-with-ring.jpg'
+          alt='Wedding ring nestled into a bridal bouquet.'
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <section>
+          <img
+            src='/images/antique-wedding-venue.jpg'
+            alt='Wedding aisle with white rose bouquets on either side.'
+          />
+          <h1>Plan your wedding with us</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            volutpat, neque ac scelerisque convallis, dui odio mattis lorem, eu
+            venenatis ante nisl vel mi. Praesent maximus laoreet augue, eget
+            dignissim dui pulvinar eu. Fusce accumsan ipsum a enim interdum
+            finibus. Aenean non lacinia dolor, at rutrum sapien. Nam elementum
+            nisl tincidunt ex consequat, nec feugiat velit aliquet.
+          </p>
+          <Link href='#'>Book An Appointment</Link>
+        </section>
+
+        <section>
+          <h2>See how our services can make your day</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            volutpat, neque ac scelerisque convallis, dui odio mattis lorem, eu
+            venenatis ante nisl vel mi. Praesent maximus laoreet augue, eget
+            dignissim dui pulvinar eu. Fusce accumsan ipsum a enim interdum
+            finibus. Aenean non lacinia dolor, at rutrum sapien. Nam elementum
+            nisl tincidunt ex consequat, nec feugiat velit aliquet.
+          </p>
+          <Link href='/services'>View Our Services</Link>
+        </section>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
-  );
+  )
 }
